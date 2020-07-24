5c29e619d2e743c5f27067d107451ca786e1f406
ajwiebe <66749375+ajwiebe@users.noreply.github.com>
2020-07-24 16:37:01 -0400

    Add files via upload

--- Developing Data Products Week 4 Project/ui.R
+++ Developing Data Products Week 4 Project/ui.R
@@ -6,7 +6,7 @@ ui <- shinyUI(fluidPage(
       numericInput("numeric2", "sd", value = 0, min = -100, max = 100, step = 1), 
     ), 
     mainPanel(
-      h3("Plot"), 
+      h3("Plot"), h6("This application takes a mean and a standard deviation (sd) and simulates a histogram of 100,000 random normals with those perameters. It also overlays a normal bell curve to the distribution."),
       plotOutput("plot1")
     )
   )
